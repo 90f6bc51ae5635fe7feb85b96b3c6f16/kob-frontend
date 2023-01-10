174bebb24dec071472a71a768d99be897f314e29
warayutpetch <31350256+warayutpetch@users.noreply.github.com>
2023-01-11 01:45:36 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,23 @@ import member from './controllers/member.js'
 
 const router = Router()
 
+router.use((req, res, next) => {
+    const origin = req.get('origin')
+    res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Headers', '*')
+  
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma')
+    res.header('Access-Control-Allow-Headers', 'x-access-token,Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma')
+  
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(204)
+    } else {
+      next()
+    }
+  })
+
 // Add USERS Routes
 router.use(member)
 
