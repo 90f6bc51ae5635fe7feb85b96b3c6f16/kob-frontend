0d23b98bf56e72361898fe25dcdf75d24944594a
NetogeNeko <33535186+Fatherfish@users.noreply.github.com>
2023-02-17 11:27:19 +0700

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,11 +1,29 @@
 export const state = () => ({
-
+  app_config: {
+    is_order_able: false,
+  },
 })
 
 export const mutations = {
-
+  setAppConfig(state, app_config) {
+    state.app_config = app_config
+  },
 }
 
 export const getters = {
+  getAppConfig(state) {
+    return state.app_config
+  },
+}
 
+export const actions = {
+  async nuxtServerInit({ commit, }, { app }) {
+    try {
+      const app_config = await app.$productService.product.getProductCategoryBy();
+
+      if (app_config.data.length) commit('setAppConfig', app_config.data[0])
+    } catch (error) {
+      console.error('nuxtServerInit', error)
+    }
+  }
 }
