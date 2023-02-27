f7f79983a389cfbc409a5f54f2d48873ecaafe7b
warayutpetch <31350256+warayutpetch@users.noreply.github.com>
2023-02-27 10:25:54 +0700

    Update index.js

--- store/index.js
+++ store/index.js
@@ -19,9 +19,10 @@ export const getters = {
 export const actions = {
   async nuxtServerInit({ commit, }, { app }) {
     try {
-      const app_config = await app.$productService.product.getProductCategoryBy();
+      const app_config = await app.$companyService.company.getCompany();
 
       if (app_config.data.length) commit('setAppConfig', app_config.data[0])
+      console.log(app_config.data);
     } catch (error) {
       console.error('nuxtServerInit', error)
     }
