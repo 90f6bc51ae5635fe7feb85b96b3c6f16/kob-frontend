f06344ddbeff1342dec48af514e7d5536ecd2c40
panupong4565 <121145483+panupong4565@users.noreply.github.com>
2023-04-05 14:13:00 +0700

    show_Video_from_Youtube

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -2,7 +2,7 @@
   <div class="mt-4">
     <h4>เกี่ยวกับเรา</h4>
     <hr />
-    <div v-html="company.company_info_about_us"></div>
+    <div class="content" v-html="filteredHtml"></div>
   </div>
 </template>
 
@@ -10,9 +10,20 @@
 export default {
   async asyncData({ $companyService }) {
     const company = await $companyService.company.getCompany();
+    const url = company.data[0].company_info_about_us;
+
+    let filteredHtml = url.replace(
+      /<oembed\s+[^>]*?url="https:\/\/(?:www\.)?youtube\.com\/watch\?v=([\w-]+).*?<\/oembed>/,
+
+      `<div class="embed-responsive embed-responsive-16by9">
+      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>
+      </div>`
+    );
 
     return {
       company: company.data ? company.data[0] : [],
+
+      filteredHtml,
     };
   },
 };
