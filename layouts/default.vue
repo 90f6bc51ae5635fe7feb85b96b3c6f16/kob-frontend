e36a315352dba4293f36907e4ccdf38a003e0f2a
Maxky2208 <maxky2208@gmail.com>
2025-11-03 11:14:49 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
   <div style="background-color: #f5f5f5">
-    <Navbar />
+<!--     <Navbar />
     <b-container
       fluid="lg"
       class="px-lg-3 py-lg-1"
       style="background-color: #ffffff"
-    >
+    > -->
       <Nuxt />
-      <SideBar />
+<!--       <SideBar />
     </b-container>
     <Advantage />
-    <Footer />
+    <Footer /> -->
   </div>
 </template>
 
