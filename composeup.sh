fc7c10218feaf145ac33614695a3a72290c8a108
artit16858 <maxky2208@gmail.com>
2025-11-14 10:41:42 +0700

    update

--- composeup.sh
+++ composeup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #docker build -f Dockerfile  -t km-kob-frontend-1 .
 
+docker login habor.rvvcs.com/revelsoft -u readonly -p tRwu9K95z2kE
+
+docker pull habor.rvvcs.com/revelsoft/auth-km-kob-frontend:v1.01
+
 docker-compose  up -d
